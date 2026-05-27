ed27e17067926ce39a08ad7f851b6efce15ec614
Seezo0 <112080273+Seezo0@users.noreply.github.com>
2026-05-27 14:57:17 +0100

    Update arrayOperations.js

--- js-challenges/arrayOperations.js
+++ js-challenges/arrayOperations.js
@@ -1,40 +1,27 @@
 let array = [3, 11, 7, 2, 9, 10];
 
-function maximum(array) {
-    let sort = array.sort(function(a, b){return a - b});
-    console.log("The maximum number on this array: " + sort[array.length - 1])
-};
-
-maximum(array);
-
-function minimum(array) {
-    let sort = array.sort(function(b, a){return b - a});
-    console.log("The minimum number on this array: " + sort[array.length - (array.length)])
-};
-
-minimum(array);
-
-function sum(array) {
-    let values = 0;
+function arrayOperations(array) {
+    let sortedArray = array.sort(function(a, b){return a - b});
     
-    for (let i = 0; i < array.length; i++) {
-      values = array[i] + values;
-    };
+    function maximum(array) {
+        console.log("The maximum number: " + sortedArray[sortedArray.length - 1])
+    }
+    maximum(sortedArray)
 
-    console.log("The sum of this array: " + values);
-};
+    function minimum(array) {
+        console.log("The lowest number: " + sortedArray[0])
+    }
+    minimum(sortedArray)
 
-sum(array);
-
-function average(array) {
-    let values = 0;
-
-    for (let i = 0; i < array.length; i++) {
-      values = array[i] + values;
-    };
-
-    let calculate = values / array.length
-    console.log("The average of this array: " + calculate);
-};
-
-average(array);
+    function sumAndAverage(array) {
+        let values = 0;
+        for (let i = 0; i < array.length; i++) {
+            values = array[i] + values
+        }
+        console.log("The sum of numbers: " + values)
+        let average = values / array.length
+        console.log("The average of numbers: " + average)
+    }
+    sumAndAverage(sortedArray)
+}
+arrayOperations(array)
