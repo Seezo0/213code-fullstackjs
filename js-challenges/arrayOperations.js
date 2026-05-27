48a26bf68eb914f343d2d477682398bc42debf8d
Seezo0 <112080273+Seezo0@users.noreply.github.com>
2026-05-27 14:57:43 +0100

    Update arrayOperations.js

--- js-challenges/arrayOperations.js
+++ js-challenges/arrayOperations.js
@@ -4,24 +4,24 @@ function arrayOperations(array) {
     let sortedArray = array.sort(function(a, b){return a - b});
     
     function maximum(array) {
-        console.log("The maximum number: " + sortedArray[sortedArray.length - 1])
-    }
-    maximum(sortedArray)
+        console.log("The maximum number: " + sortedArray[sortedArray.length - 1]);
+    };
+    maximum(sortedArray);
 
     function minimum(array) {
-        console.log("The lowest number: " + sortedArray[0])
-    }
-    minimum(sortedArray)
+        console.log("The lowest number: " + sortedArray[0]);
+    };
+    minimum(sortedArray);
 
     function sumAndAverage(array) {
         let values = 0;
         for (let i = 0; i < array.length; i++) {
             values = array[i] + values
-        }
-        console.log("The sum of numbers: " + values)
-        let average = values / array.length
-        console.log("The average of numbers: " + average)
-    }
-    sumAndAverage(sortedArray)
-}
-arrayOperations(array)
+        };
+        console.log("The sum of numbers: " + values);
+        let average = values / array.length;
+        console.log("The average of numbers: " + average);
+    };
+    sumAndAverage(sortedArray);
+};
+arrayOperations(array);
